79902bc2854feea6ad95c84a6118d9d365b22595
German Mesa <german.mesa@sap.com>
2020-05-20 17:25:35 +0200

    Linear model with callbacks

--- keras_linear.py
+++ keras_linear.py
@@ -2,30 +2,42 @@ import os
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
-callbacks = [
-    tf.keras.callbacks.EarlyStopping(
-        # Stop training when `val_loss` is no longer improving
-        monitor='val_loss',
-        # "no longer improving" being defined as "no better than 1e-5 less"
-        min_delta=1e-5,
-        # "no longer improving" being further defined as "for at least 5 epochs"
-        patience=5,
-        verbose=1),
-    tf.keras.callbacks.TensorBoard(
-        log_dir=os.path.join(os.getcwd(), 'log'),
-        # How often to log histogram visualizations
-        histogram_freq=0,
-        # How often to log embedding visualizations
-        embeddings_freq=0,
-        # How often to write logs (default: once per epoch)
-        update_freq='epoch')
-]
+from time import time
 
 
 # Function is y = 3x + 1
 def linear_function(x):
     return 3 * x + 1
+
+
+checkpoint_path = os.path.join(os.getcwd(), 'checkpoint/') + 'cp-{epoch:04d}.ckpt'
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(
+        # Stop training when `val_loss` is no longer improving
+        monitor='val_loss',
+        # "no longer improving" being defined as "no better than 1e-3 less"
+        min_delta=1e-3,
+        # "no longer improving" being further defined as "for at least 5 epochs"
+        patience=5,
+        verbose=1),
+    tf.keras.callbacks.TensorBoard(
+        log_dir=os.path.join(os.getcwd(), 'log/{}'.format(time())),
+        # How often to log histogram visualizations
+        histogram_freq=0,
+        # How often to log embedding visualizations
+        embeddings_freq=0,
+        # How often to write logs (default: once per epoch)
+        update_freq='epoch'),
+    tf.keras.callbacks.ModelCheckpoint(
+        # Create a callback that saves the model's weights
+        filepath=checkpoint_path,
+        save_weights_only=True,
+        # Save the model every 5 epochs
+        period=5,
+        verbose=1)
+]
 
 
 def build_model():
@@ -42,25 +54,6 @@ def build_model():
     return model
 
 
-def plot_history(history):
-    plt.figure()
-
-    plt.ylim([0, 0.5])
-
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Abs Error / Mean Squared Error')
-    plt.legend()
-
-    plt.plot(history.epoch,
-             np.array(history.history['mean_absolute_error']),
-             label='Mean Absolute Error')
-    plt.plot(history.epoch,
-             np.array(history.history['mean_squared_error']),
-             label='Mean Squared Error')
-
-    plt.show()
-
-
 def main():
     xs = np.linspace(start=-10.0, stop=10.0, num=1000)
     ys = np.array(linear_function(xs), dtype=float)
@@ -71,8 +64,6 @@ def main():
                         callbacks=callbacks,
                         validation_split=0.1,
                         epochs=50)
-
-    # plot_history(history)
 
     print(model.predict([10.0, 5.0]))
 
